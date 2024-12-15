53f578e87f82f0d5f93fa8fa234d81e98a093fc6
flashhhhh <nguyenchithanhpro196@gmail.com>
2024-12-15 16:18:17 +0700

    Add CSS

--- app.py
+++ app.py
@@ -11,3 +11,15 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/units/length", response_class=JSONResponse)
+def listLengthUnits():
+    return {"units": ["Meter", "Kilometer", "Centimeter", "Millimeter", "Micrometer", "Nanometer", "Mile", "Yard", "Foot", "Inch", "Light Year"]}
+
+@app.get("/units/temperature", response_class=JSONResponse)
+def listTemperatureUnits():
+    return {"units": ["Celsius", "Fahrenheit", "Kelvin"]}
+
+@app.get("/units/weight", response_class=JSONResponse)
+def listWeightUnits():
+    return {"units": ["Kilogram", "Gram", "Milligram", "Microgram", "Ton", "Pound", "Ounce", "Carat"]}
